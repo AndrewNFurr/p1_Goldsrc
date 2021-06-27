576d4c6a71ab45e023156a95a53fad083e53db85
Joshua Faaborg <jlfaaborg@gmail.com>
2021-06-27 15:35:28 -0400

    Started Functionality

--- Project 0/force-app/main/default/aura/P2_Account_Info/P2_Account_InfoController.js
+++ Project 0/force-app/main/default/aura/P2_Account_Info/P2_Account_InfoController.js
@@ -1,5 +1,6 @@
 ({
 	init : function(component, event, helper) {
 		helper.init(component);
+        console.log(component.get("v.recordId"))
 	}
 })
--- Project 0/force-app/main/default/aura/P2_Account_Info/P2_Account_InfoHelper.js
+++ Project 0/force-app/main/default/aura/P2_Account_Info/P2_Account_InfoHelper.js
@@ -1,5 +1,11 @@
 ({
 	init : function(component) {
 		let action = component.get("c.getAccountIds");
+        action.setCallback(this, function(response) {
+           let res = response.getReturnValue("account");
+           console.log(res);
+           component.set("v.recordId", res.Id);
+        })
+        $A.enqueueAction(action);
 	}
 })
